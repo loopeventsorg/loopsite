1c2153d9ed291a2317613047d58912512cabdbde
loopeventsorg <ryan@loopevents.org>
2025-07-28 16:32:59 -0500

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,444 @@
+var map_cfg = {
+ "mapWidth": "100%",
+ "mapHeight": 410,
+ "shadowWidth": 2,
+ "shadowOpacity": 0.2,
+ "shadowColor": "black",
+ "shadowX": 0,
+ "shadowY": 0,
+ "iPhoneLink": true,
+ "isNewWindow": false,
+ "zoomEnable": false,
+ "zoomEnableControls": false,
+ "zoomIgnoreMouseScroll": false,
+ "zoomMax": 8,
+ "zoomStep": 0.8,
+ "borderColor": "#ffffff",
+ "borderColorOver": "#ffffff",
+ "borderOpacity": 0.5,
+ "nameColor": "#ffffff",
+ "nameColorOver": "#ffffff",
+ "nameFontSize": "10px",
+ "nameFontWeight": "bold",
+ "nameStroke": false,
+ "nameStrokeColor": "#000000",
+ "nameStrokeColorOver": "#000000",
+ "nameStrokeWidth": 1.5,
+ "nameStrokeOpacity": 0.5,
+ "overDelay": 300,
+ "nameAutoSize": false,
+ "tooltipOnHighlightIn": false,
+ "freezeTooltipOnClick": false,
+ "mapId": "vcg",
+ "map_data": {
+  "st1": {
+   "link": "",
+   "name": "Alabama",
+   "color": "#000000",
+   "comment": "",
+   "colorOver": "#E06666",
+   "shortname": "AL"
+  },
+  "st2": {
+   "link": "",
+   "name": "Alaska",
+   "color": "#000000",
+   "comment": "",
+   "colorOver": "#E06666",
+   "shortname": "AK "
+  },
+  "st3": {
+   "link": "",
+   "name": "Arizona",
+   "color": "#000000",
+   "comment": "",
+   "colorOver": "#E06666",
+   "shortname": "AZ "
+  },
+  "st4": {
+   "link": "",
+   "name": "Arkansas",
+   "color": "#000000",
+   "comment": "",
+   "colorOver": "#E06666",
+   "shortname": "AR"
+  },
+  "st5": {
+   "link": "",
+   "name": "California",
+   "color": "#000000",
+   "comment": "",
+   "colorOver": "#E06666",
+   "shortname": "CA"
+  },
+  "st6": {
+   "link": "",
+   "name": "Colorado",
+   "color": "#000000",
+   "comment": "",
+   "colorOver": "#E06666",
+   "shortname": "CO"
+  },
+  "st7": {
+   "link": "",
+   "name": "Connecticut",
+   "color": "#000000",
+   "comment": "",
+   "colorOver": "#E06666",
+   "shortname": "CT"
+  },
+  "st8": {
+   "link": "",
+   "name": "Delaware",
+   "color": "#000000",
+   "comment": "",
+   "colorOver": "#E06666",
+   "shortname": "DE"
+  },
+  "st9": {
+   "link": "",
+   "name": "District of Columbia",
+   "color": "#000000",
+   "comment": "",
+   "colorOver": "#E06666",
+   "shortname": "DC"
+  },
+  "st10": {
+   "link": "",
+   "name": "Florida",
+   "color": "#000000",
+   "comment": "",
+   "colorOver": "#E06666",
+   "shortname": "FL"
+  },
+  "st11": {
+   "link": "",
+   "name": "Georgia",
+   "color": "#000000",
+   "comment": "",
+   "colorOver": "#E06666",
+   "shortname": "GA"
+  },
+  "st12": {
+   "link": "",
+   "name": "Hawaii",
+   "color": "#000000",
+   "comment": "",
+   "colorOver": "#E06666",
+   "shortname": "HI"
+  },
+  "st13": {
+   "link": "",
+   "name": "Idaho",
+   "color": "#000000",
+   "comment": "",
+   "colorOver": "#E06666",
+   "shortname": "ID"
+  },
+  "st14": {
+   "link": "",
+   "name": "Illinois",
+   "color": "#000000",
+   "comment": "",
+   "colorOver": "#E06666",
+   "shortname": "IL"
+  },
+  "st15": {
+   "link": "",
+   "name": "Indiana",
+   "color": "#000000",
+   "comment": "",
+   "colorOver": "#E06666",
+   "shortname": "IN"
+  },
+  "st16": {
+   "link": "",
+   "name": "Iowa",
+   "color": "#000000",
+   "comment": "",
+   "colorOver": "#E06666",
+   "shortname": "IA"
+  },
+  "st17": {
+   "link": "",
+   "name": "Kansas",
+   "color": "#000000",
+   "comment": "",
+   "colorOver": "#E06666",
+   "shortname": "KS"
+  },
+  "st18": {
+   "link": "",
+   "name": "Kentucky",
+   "color": "#000000",
+   "comment": "",
+   "colorOver": "#E06666",
+   "shortname": "KY"
+  },
+  "st19": {
+   "link": "",
+   "name": "Louisiana",
+   "color": "#000000",
+   "comment": "",
+   "colorOver": "#E06666",
+   "shortname": "LA"
+  },
+  "st20": {
+   "link": "",
+   "name": "Maine",
+   "color": "#000000",
+   "comment": "",
+   "colorOver": "#E06666",
+   "shortname": "ME"
+  },
+  "st21": {
+   "link": "",
+   "name": "Maryland",
+   "color": "#000000",
+   "comment": "",
+   "colorOver": "#E06666",
+   "shortname": "MD"
+  },
+  "st22": {
+   "link": "",
+   "name": "Massachusetts",
+   "color": "#000000",
+   "comment": "",
+   "colorOver": "#E06666",
+   "shortname": "MA"
+  },
+  "st23": {
+   "link": "",
+   "name": "Michigan",
+   "color": "#000000",
+   "comment": "",
+   "colorOver": "#E06666",
+   "shortname": "MI"
+  },
+  "st24": {
+   "link": "",
+   "name": "Minnesota",
+   "color": "#000000",
+   "comment": "",
+   "colorOver": "#E06666",
+   "shortname": "MN"
+  },
+  "st25": {
+   "link": "",
+   "name": "Mississippi",
+   "color": "#000000",
+   "comment": "",
+   "colorOver": "#E06666",
+   "shortname": "MS"
+  },
+  "st26": {
+   "link": "",
+   "name": "Missouri",
+   "color": "#000000",
+   "comment": "",
+   "colorOver": "#E06666",
+   "shortname": "MO"
+  },
+  "st27": {
+   "link": "",
+   "name": "Montana",
+   "color": "#000000",
+   "comment": "",
+   "colorOver": "#E06666",
+   "shortname": "MT"
+  },
+  "st28": {
+   "link": "",
+   "name": "Nebraska",
+   "color": "#000000",
+   "comment": "",
+   "colorOver": "#E06666",
+   "shortname": "NE"
+  },
+  "st29": {
+   "link": "",
+   "name": "Nevada",
+   "color": "#000000",
+   "comment": "",
+   "colorOver": "#E06666",
+   "shortname": "NV"
+  },
+  "st30": {
+   "link": "",
+   "name": "New Hampshire",
+   "color": "#000000",
+   "comment": "",
+   "colorOver": "#E06666",
+   "shortname": "NH"
+  },
+  "st31": {
+   "link": "",
+   "name": "New Jersey",
+   "color": "#000000",
+   "comment": "",
+   "colorOver": "#E06666",
+   "shortname": "NJ"
+  },
+  "st32": {
+   "link": "",
+   "name": "New Mexico",
+   "color": "#000000",
+   "comment": "",
+   "colorOver": "#E06666",
+   "shortname": "NM"
+  },
+  "st33": {
+   "link": "",
+   "name": "New York",
+   "color": "#000000",
+   "comment": "",
+   "colorOver": "#E06666",
+   "shortname": "NY"
+  },
+  "st34": {
+   "link": "",
+   "name": "North Carolina",
+   "color": "#000000",
+   "comment": "",
+   "colorOver": "#E06666",
+   "shortname": "NC"
+  },
+  "st35": {
+   "link": "",
+   "name": "North Dakota",
+   "color": "#000000",
+   "comment": "",
+   "colorOver": "#E06666",
+   "shortname": "ND"
+  },
+  "st36": {
+   "link": "",
+   "name": "Ohio",
+   "color": "#000000",
+   "comment": "",
+   "colorOver": "#E06666",
+   "shortname": "OH"
+  },
+  "st37": {
+   "link": "",
+   "name": "Oklahoma",
+   "color": "#000000",
+   "comment": "",
+   "colorOver": "#E06666",
+   "shortname": "OK"
+  },
+  "st38": {
+   "link": "",
+   "name": "Oregon",
+   "color": "#000000",
+   "comment": "",
+   "colorOver": "#E06666",
+   "shortname": "OR"
+  },
+  "st39": {
+   "link": "",
+   "name": "Pennsylvania",
+   "color": "#000000",
+   "comment": "",
+   "colorOver": "#E06666",
+   "shortname": "PA"
+  },
+  "st40": {
+   "link": "",
+   "name": "Rhode Island",
+   "color": "#000000",
+   "comment": "",
+   "colorOver": "#E06666",
+   "shortname": "RI"
+  },
+  "st41": {
+   "link": "",
+   "name": "South Carolina",
+   "color": "#000000",
+   "comment": "",
+   "colorOver": "#E06666",
+   "shortname": "SC"
+  },
+  "st42": {
+   "link": "",
+   "name": "South Dakota",
+   "color": "#000000",
+   "comment": "",
+   "colorOver": "#E06666",
+   "shortname": "SD"
+  },
+  "st43": {
+   "link": "",
+   "name": "Tennessee",
+   "color": "#000000",
+   "comment": "",
+   "colorOver": "#E06666",
+   "shortname": "TN"
+  },
+  "st44": {
+   "link": "",
+   "name": "Texas",
+   "color": "#000000",
+   "comment": "",
+   "colorOver": "#E06666",
+   "shortname": "TX"
+  },
+  "st45": {
+   "link": "",
+   "name": "Utah",
+   "color": "#000000",
+   "comment": "",
+   "colorOver": "#E06666",
+   "shortname": "UT"
+  },
+  "st46": {
+   "link": "",
+   "name": "Vermont",
+   "color": "#000000",
+   "comment": "",
+   "colorOver": "#E06666",
+   "shortname": "VT"
+  },
+  "st47": {
+   "link": "",
+   "name": "Virginia",
+   "color": "#000000",
+   "comment": "",
+   "colorOver": "#E06666",
+   "shortname": "VA"
+  },
+  "st48": {
+   "link": "",
+   "name": "Washington",
+   "color": "#000000",
+   "comment": "",
+   "colorOver": "#E06666",
+   "shortname": "WA"
+  },
+  "st49": {
+   "link": "",
+   "name": "West Virginia",
+   "color": "#000000",
+   "comment": "",
+   "colorOver": "#E06666",
+   "shortname": "WV"
+  },
+  "st50": {
+   "link": "",
+   "name": "Wisconsin",
+   "color": "#000000",
+   "comment": "",
+   "colorOver": "#E06666",
+   "shortname": "WI"
+  },
+  "st51": {
+   "link": "",
+   "name": "Wyoming",
+   "color": "#000000",
+   "comment": "",
+   "colorOver": "#E06666",
+   "shortname": "WY"
+  }
+ },
+ "points": {}
+};
